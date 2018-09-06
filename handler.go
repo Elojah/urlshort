e9b88b078f023cd62c3a417ef5979abbd1a6b9f7
elojah <swingcastor@gmail.com>
2018-09-06 18:39:57 +0200

    add xml and json handlers

--- handler.go
+++ handler.go
@@ -1,8 +1,28 @@
 package urlshort
 
 import (
+	"encoding/json"
 	"net/http"
+
+	yaml "gopkg.in/yaml.v2"
 )
+
+// Redirection represents a path redirection /foo to a defined URL.
+type Redirection struct {
+	Path string `json:"path", yaml:"path"`
+	URL  string `json:"url", yaml:"url"`
+}
+
+type Redirections []Redirection
+
+// Map converts a redirection slice into a MapHandler parameters.
+func (rs Redirections) Map() map[string]string {
+	result := make(map[string]string, len(rs))
+	for _, r := range rs {
+		result[r.Path] = r.URL
+	}
+	return result
+}
 
 // MapHandler will return an http.HandlerFunc (which also
 // implements http.Handler) that will attempt to map any
@@ -11,8 +31,13 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		if url, ok := pathsToUrls[r.URL.Path]; ok {
+			http.Redirect(w, r, url, http.StatusPermanentRedirect)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -32,6 +57,18 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	var redirections Redirections
+	if err := yaml.Unmarshal(yml, &redirections); err != nil {
+		return nil, err
+	}
+	return MapHandler(redirections.Map(), fallback), nil
+}
+
+// JSONHandler will parse provided json and returns a redirection handler.
+func JSONHandler(raw []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var redirections Redirections
+	if err := json.Unmarshal(raw, &redirections); err != nil {
+		return nil, err
+	}
+	return MapHandler(redirections.Map(), fallback), nil
 }
